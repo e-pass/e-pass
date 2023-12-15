4fa7c16768a7b125a8875e1b4898bca1aee492ee
maksim.lazko <m.lazko@naveksoft.com>
2023-12-16 01:20:32 +0300

    changed api prefix

--- sections/urls.py
+++ sections/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from sections.views import SectionViewSet
 
 router = routers.DefaultRouter()
-router.register(prefix='sections', viewset=SectionViewSet, basename='section_api')
+router.register(prefix='section', viewset=SectionViewSet, basename='section_api')
 
 urlpatterns = router.urls
