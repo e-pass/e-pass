4c26c72673186557f6c58e9f85408b4fa513a26f
maksim.lazko <m.lazko@naveksoft.com>
2023-12-16 00:48:52 +0300

    updated serializr

--- section/serializer.py
+++ section/serializer.py
@@ -2,13 +2,34 @@ from rest_framework import serializers
 
 from section.models import SectionModel
 from users.models import UserModel
+from users.serializer import UserModelSerializer
 
 
 class SectionSerializer(serializers.ModelSerializer):
-    owner = serializers.PrimaryKeyRelatedField(queryset=UserModel.objects.all())
-    trainers = serializers.PrimaryKeyRelatedField(queryset=UserModel.objects.all(), many=True, required=False)
-    students = serializers.PrimaryKeyRelatedField(queryset=UserModel.objects.all(), many=True, required=False)
+    owner_id = serializers.PrimaryKeyRelatedField(
+        source='owner',
+        queryset=UserModel.objects.all(),
+        write_only=True
+    )
+    trainers_ids = serializers.PrimaryKeyRelatedField(
+        source='trainers',
+        queryset=UserModel.objects.filter(is_trainer=True),
+        write_only=True,
+        required=False,
+        many=True
+    )
+    students_ids = serializers.PrimaryKeyRelatedField(
+        source='students',
+        queryset=UserModel.objects.all(),
+        write_only=True,
+        required=False,
+        many=True
+    )
+
+    owner = UserModelSerializer(read_only=True)
+    trainers = UserModelSerializer(many=True, read_only=True)
+    students = UserModelSerializer(many=True, read_only=True)
 
     class Meta:
         model = SectionModel
-        fields = ('id', 'name', 'owner', 'trainers', 'students', 'created_at', 'updated_at',)
+        fields = '__all__'
