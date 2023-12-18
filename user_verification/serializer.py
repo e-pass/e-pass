c817db1e53d7c6d8222a2ea845c14da96118e9e1
maksim.lazko <m.lazko@naveksoft.com>
2023-12-18 23:47:54 +0300

    added phone_number field validator

--- user_verification/serializer.py
+++ user_verification/serializer.py
@@ -13,28 +13,31 @@ class ConfirmationCodeSerializer(serializers.Serializer):
 
     def create(self, validated_data: dict) -> dict:
         phone_number = validated_data.get('phone_number')
-
-        user = UserModel.objects.get(phone_number=phone_number)
         code = self.__generate_confirmation_code()
+        user = UserModel.objects.get(phone_number=phone_number)
         ConfirmationCodeModel.objects.create(user=user, code=f'{code}')
+        # TODO: Здесь должна быть логика отправки смс с кодом на указанный номер
+        return {'code': code}
 
-        # Здесь должна быть логика отправки смс с кодом на указанный номер
-
-        return {'user': user, 'code': code}
+    @staticmethod
+    def validate_phone_number(phone_number: str) -> str:
+        if not UserModel.objects.filter(phone_number=phone_number).exists():
+            raise serializers.ValidationError('Пользователя с введенным номером телефона не существует')
+        return phone_number
 
     @staticmethod
     def __generate_confirmation_code() -> int:
-        return random.randint(111111, 999999)
+        return random.randint(1111, 9999)
 
 
 class VerifyCodeSerializer(serializers.Serializer):
-    code = serializers.CharField(min_length=6, max_length=6, required=True)
+    code = serializers.CharField(min_length=4, max_length=4, required=True)
 
     @staticmethod
     def validate_code(code: str) -> str:
         code_model = ConfirmationCodeModel.get_confirmation_code(code=code)
 
         if not code_model or code_model.is_expired() or not code_model.is_valid:
-            raise serializers.ValidationError('Invalid or expired code')
+            raise serializers.ValidationError('Неправильный код или истек срок действия')
 
         return code
