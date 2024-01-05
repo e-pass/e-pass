9e219adc4ae3ae4f07450edc7b6337325901be26
maksim.lazko <m.lazko@naveksoft.com>
2024-01-05 23:03:29 +0300

    rm log levels

--- ePass/settings.py
+++ ePass/settings.py
@@ -262,7 +262,7 @@ LOGGING = {
     },
     'handlers': {
         'django_file': {
-            'level': env('DJANGO_LOG_LEVEL'),
+            'level': env('LOG_LEVEL'),
             'class': 'logging.handlers.RotatingFileHandler',
             'filename': os.path.join(BASE_DIR, 'logs', 'django.log'),
             'maxBytes': 1024 * 1024 * 3,  # 3 MB
@@ -270,7 +270,7 @@ LOGGING = {
             'formatter': 'simple',
         },
         'requests_file': {
-            'level': env('API_REQUESTS_LOG_LEVEL'),
+            'level': env('LOG_LEVEL'),
             'class': 'logging.handlers.RotatingFileHandler',
             'filename': os.path.join(BASE_DIR, 'logs', 'request.log'),
             'maxBytes': 1024 * 1024 * 3,  # 3 MB
@@ -278,7 +278,7 @@ LOGGING = {
             'formatter': 'simple',
         },
         'database_file': {
-            'level': env('DB_LOG_LEVEL'),
+            'level': env('LOG_LEVEL'),
             'class': 'logging.handlers.RotatingFileHandler',
             'filename': os.path.join(BASE_DIR, 'logs', 'db.log'),
             'maxBytes': 1024 * 1024 * 3,  # 3 MB
@@ -286,7 +286,7 @@ LOGGING = {
             'formatter': 'simple',
         },
         'celery_file': {
-            'level': env('CELERY_LOG_LEVEL'),
+            'level': env('LOG_LEVEL'),
             'class': 'logging.handlers.RotatingFileHandler',
             'filename': os.path.join(BASE_DIR, 'logs', 'celery.log'),
             'maxBytes': 1024 * 1024 * 3,  # 3 MB
@@ -297,21 +297,21 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['django_file'],
-            'level': env('DJANGO_LOG_LEVEL'),
+            'level': env('LOG_LEVEL'),
             'propagate': True,
         },
         'django.request': {
             'handlers': ['requests_file'],
-            'level': env('API_REQUESTS_LOG_LEVEL'),
+            'level': env('LOG_LEVEL'),
             'propagate': True,
         },
         'django.db.backends': {
             'handlers': ['database_file'],
-            'level': env('DB_LOG_LEVEL'),
+            'level': env('LOG_LEVEL'),
         },
         'celery': {
             'handlers': ['celery_file'],
-            'level': env('CELERY_LOG_LEVEL'),
+            'level': env('LOG_LEVEL'),
             'propagate': True,
         },
     },
