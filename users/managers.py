5f3f4670f793d7a3a703ecd0e5c546f6e8eb747f
Alexey <87098304+Salvador-Geniy@users.noreply.github.com>
2024-01-17 20:06:46 -0300

    Update managers.py

--- users/managers.py
+++ users/managers.py
@@ -1,7 +1,6 @@
 from typing import Any, Type
 from django.db import models
 
-
 from django.contrib.auth.base_user import BaseUserManager
 from phonenumber_field.modelfields import PhoneNumberField
 
