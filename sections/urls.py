0db53b46741457425a70e8ee779103290cc35505
maksim.lazko <m.lazko@naveksoft.com>
2024-03-29 17:13:04 +0300

    refactor lessons

--- sections/urls.py
+++ sections/urls.py
@@ -3,11 +3,12 @@ from rest_framework import routers
 
 from sections.views import (GroupListCreateAPIView,
                             GroupRetrieveUpdateDestroyAPIView,
-                            LessonListCreateAPIView,
-                            LessonRetrieveUpdateDestroyAPIView, SectionViewSet)
+                            LessonViewSet,
+                            SectionViewSet)
 
 router = routers.DefaultRouter()
 router.register(prefix='section', viewset=SectionViewSet, basename='section_api')
+router.register(prefix='lesson', viewset=LessonViewSet, basename='lesson_api')
 
 urlpatterns = [
     path('section/<int:section_id>/group/', GroupListCreateAPIView.as_view(), name='group_list_create'),
@@ -16,10 +17,4 @@ urlpatterns = [
         GroupRetrieveUpdateDestroyAPIView.as_view(),
         name='group_RUD'
     ),
-    path('section/<int:section_id>/lesson/', LessonListCreateAPIView.as_view(), name='lesson_list_create'),
-    path(
-        'section/<int:section_id>/lesson/<int:lesson_id>/',
-        LessonRetrieveUpdateDestroyAPIView.as_view(),
-        name='lesson_RUD'
-    )
 ] + router.urls
